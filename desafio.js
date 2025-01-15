223589fd2a15cdd9a708e07f874c8f3fed0de29f
petrusjuca <146126655+petrusjuca@users.noreply.github.com>
2025-01-15 15:05:28 -0300

    Create desafio.js

---
+++ desafio.js
@@ -0,0 +1,8 @@
+
+let PokemonName = ["POOCHYENA", "ZIGZAGOON","DRAGONITE", "DRAGONITE", "POOCHYENA", "WURMPLE"];
+let NivelPokemon = [2,2,5,5,5,3,2];
+let HP = [13/13,13/13,25/25,24/24,15/15,7/14];
+
+console.log(PokemonName )
+
+// Petrus do Futuro por favor lembre de terminar essa Atividade eu ficarei agradecido.
